e4ba4b85c0b7bd307c1e0c37f45532f6fdd7f18b
papob.boonpat <papobboonpat@txm089557a.localdomain>
2023-09-09 00:53:26 +0700

    home page with Header

---
+++ components/customHeader.tsx
@@ -0,0 +1,72 @@
+import Colors from "@/constants/Colors";
+import { Ionicons } from "@expo/vector-icons";
+import React from "react";
+import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+const CustomHeader = () => {
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <TouchableOpacity onPress={() => {}}>
+          <Image
+            style={styles.bike}
+            source={require("@/assets/images/bike.png")}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.titleContainer}>
+          <Text style={styles.title}>Derivery · Now</Text>
+          <View style={styles.locationName}>
+            <Text style={styles.subTitle}>London</Text>
+            <Ionicons name="chevron-down" size={20} color={Colors.primary} />
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.profileButton}>
+          <Ionicons name="person-outline" size={20} color={Colors.primary} />
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  container: {
+    height: 60,
+    backgroundColor: "white",
+    flexDirection: "row",
+    gap: 20,
+    alignItems: "center",
+    justifyContent: "space-between",
+    paddingHorizontal: 20,
+  },
+  bike: {
+    width: 30,
+    height: 30,
+  },
+  titleContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 14,
+    color: Colors.medium,
+  },
+  profileButton: {
+    backgroundColor: Colors.lightGrey,
+    padding: 10,
+    borderRadius: 50,
+  },
+  subTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  locationName: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+});
+
+export default CustomHeader;
